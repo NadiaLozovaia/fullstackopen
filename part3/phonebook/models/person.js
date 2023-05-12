9c95ff27a3cbc3156fe2c617ef88d3cc2cf1b59e
Nadia Lozovaia <nadin.lozovaia@gmail.com>
2023-05-12 21:02:11 +0300

    change personSchema

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -27,10 +27,10 @@ const personSchema = new mongoose.Schema({
     },
     number: {
         type: String,
-        minLength: 8,
+        minLength: 9,
         validate: {
             validator: function (v) {
-                return /\d{2,3}-\d{4,10}/.test(v)
+                return /\d{2,3}-\d{5,10}/.test(v)
             },
             message: props => `${props.value} is not a valid phone number!`
         },
